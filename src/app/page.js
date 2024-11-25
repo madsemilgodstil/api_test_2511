0fc7e497e69ee8edd7e9e15c32772349803dedec
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-25 13:14:33 +0100

    ffff

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
 import Image from 'next/image'
 
+export function data () {
+  const endpoint = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+  async function fetchData () {
+    const response = await fetch(endpoint, {
+      headers: {
+        apikey: key
+      }
+    })
+    const data = await response.json()
+    return data
+  }
+}
+
 export default function Home () {
-  const subscriber = ''
+  const subscriber = 'data.navn'
 
   return (
     <div>
