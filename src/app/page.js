e3a557ca0046ed1cb3680aec03c83ca52e54cc47
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-25 13:37:29 +0100

    ddd

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,5 @@
 import Image from 'next/image'
 
 export default function Home () {
-  const subscriber = 'data.navn'
-
-  return (
-    <div>
-      <h1>{subscriber}</h1>
-    </div>
-  )
+  return <div></div>
 }
