68016e8cf84ad4565a92b79a29dbb74a8c9e3682
madsemilgodstil <mads93n2@stud.kea.dk>
2024-11-25 14:20:01 +0100

    jjj

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,15 +1,13 @@
-const url = 'NEXT_PUBLIC_SUPABASE_URL'
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL
 
-const api = 'NEXT_PUBLIC_SUPABASE_ANON_KEY'
+const api = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+const headersList = {
+  Accept: 'application/json',
+  apikey: api,
+  prefer: 'return=representation'
+}
 
 async function getSubs () {
-  let headersList = {
-    Accept: '*/*',
-    apikey: api,
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5taXlkdGZ4ZHVwZmN3aW1ncWxuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI1MzQ4MjEsImV4cCI6MjA0ODExMDgyMX0.kxXrQ2kNPWjqYOnOgvFul63nEtNXMq5eCB87yz63P0A'
-  }
-
   let response = await fetch('url', {
     method: 'GET',
     headers: headersList
